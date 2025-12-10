4a95625f0adc6bce2aaa480c46ec6c4d873dbef3
YUGRAJ MEWARA <yugrajmewara08@gmail.com>
2025-12-10 21:13:30 +0530

    Adding MergeSort In Java Language

---
+++ Sorting Algorithms/MergeSort.java
@@ -0,0 +1,69 @@
+public class MergeSort {
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Divide
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Conquer (Merge)
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy elements
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int i = 0; i < n2; i++)
+            R[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+
+        // Merge two sorted arrays
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int[] arr = { 38, 27, 43, 3, 9, 82, 10 };
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted: ");
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
